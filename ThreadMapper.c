98fd56552967189e8458cb9741c7a0050d40d6ed
Nishanth Sivakumar <nishanth@iastate.edu>
2016-02-14 23:33:31 -0600

    Modified Working version of ThreadMapper.c. Implemented Queues.

--- ThreadMapper.c
+++ ThreadMapper.c
@@ -1,7 +1,7 @@
 
 /*
  *  many2one mapping v1.0: 
- *  the library provides uthread_create, uthread_yield and uthread_exit 
+ *  the library provides enqueue_context, yield and dequeue_context 
 */
 
 #define _XOPEN_SOURCE
@@ -12,129 +12,186 @@
 #include <ucontext.h>
 #include <unistd.h>
 
+ typedef struct context_queue Queue;
+
 //declare the functions provided by the thread library 
-void uthread_create(int (*fn)());
-void uthread_exit();
-void uthread_yield();
+Queue* create_context(int (*func)());
+Queue* dequeue_context();
+void enqueue_context(Queue *context);
+void yield();
 
 /**** Application code ****/
+
+
+
+struct context_queue{
+  ucontext_t *context;
+  Queue *next;
+};
+Queue *head=NULL, *tail=NULL;
 
 int func1()
 {
   int i;
-  for(i=0;i<3;i++){
-    printf("Executing thread 1.\n");
-    sleep(1);
-    if( i==1){
-      printf("Calling yeild on thread 1\n");
-      uthread_yield();
+  printf("Sum of First 200 numbers by Thread 1 \n");
+  int sum = 0;
+  int yield_1=0,yield_2=0;
+  for(i=1;i<=200;i++){
+
+    sum = sum + i;
+    if(sum >= 100 && yield_1 == 0){
+      sleep(1);
+      yield_1 = 1;
+      printf("Sum by Thread 1 reached 100 - Yielding for Next Thread\n");
+      yield();
+    }
+    if(sum >= 200 && yield_2 == 0){
+      sleep(1);
+      yield_2 = 1;
+      printf("Sum by Thread 1 reached 200 - Yielding for Next Thread\n");
+      yield();
     }
   }
-  printf("Exiting Thread 1\n");
-  uthread_exit();
+  printf("Sum of First 200 Numbers = %d\n",sum);
+  dequeue_context();
+  if(head != NULL){
+    setcontext(head->context);
+  }
 }
 
 int func2()
 {
   int i;
-  for(i=0;i<3;i++){
-    printf("Executing thread 2.\n");
-    sleep(1);
-    if( i==1){
-      printf("Calling yeild on thread 2\n");
-      uthread_yield();
+  long product = 1;
+  printf("Product of first 20 numbers by Thread 2\n");
+  int yield_1=0,yield_2=0;
+  for(i=1;i<=20;i++){
+    product = product * i;
+    if(product > 100 && yield_1 == 0){
+      sleep(1);
+      yield_1 = 1;
+      printf("Product by Thread 2 has reached 100 - Yielding for Next Thread\n");
+      yield();
+    }
+    if(product > 500 && yield_2 == 0){
+      sleep(1);
+      yield_2 = 1;
+      printf("Product by Thread 2 has reached 500 - Yielding for Next Thread\n");
+      yield();
     }
   }
-  printf("Exiting Thread 2.\n");
-  uthread_exit();
+  printf("Product of First 20 Numbers = %ld\n",product);
+  dequeue_context();
+  if(head != NULL){
+    setcontext(head->context);
+  }
 }
 
 int func3()
 {
   int i;
-  for(i=0;i<3;i++){
-    printf("Executing thread 3.\n");
-    sleep(1);
-    if( i==1){
-      printf("Calling yeild on thread 3\n");
-      uthread_yield();
+  int even_sum = 0;
+  printf("Sum of First 100 Even Numbers by Thread 3\n");
+  for(i=1;i<=100;i=i+2){
+    even_sum = even_sum + i;
+    if(even_sum == 100){
+      sleep(1);
+      printf("Even Sum by Thread 3 Reached 100. Sum upto %d is %d\n",i,even_sum);
+      yield();
     }
   }
-  printf("Exiting Thread 3.\n");
-  uthread_exit();
+  printf("Sum of First 100 Even Numbers = %d\n",even_sum);
+  dequeue_context();
+  if(head != NULL){
+    setcontext(head->context);
+  }
+  
 }
 
 int main()
 {
-  uthread_create(func1);
-  uthread_create(func2);
-  uthread_create(func3);
-  uthread_exit();
+  //create_context(func1);
+  enqueue_context(create_context(func1));
+
+  //create_context(func2);
+  enqueue_context(create_context(func2));
+
+  //create_context(func3);
+  enqueue_context(create_context(func3));
+  
+  
+  setcontext(head->context);
 }
 
 
 /**** Thread library code ****/
 
 //define a queue of user thread records
-struct thread_info{
-  ucontext_t *ucp;
-  struct thread_info *next;
-};
-struct thread_info *head=NULL, *tail=NULL;
 
 
-void uthread_create(int (*func)())
+
+Queue* create_context(int (*func)())
 {
   //construct a thread record
-  struct thread_info *th;
-  th=(struct thread_info *)malloc(sizeof(struct thread_info));
-  th->ucp=(ucontext_t *)malloc(sizeof(ucontext_t));
-  getcontext(th->ucp); //initialize the context structure
-  th->ucp->uc_stack.ss_sp=(void *)malloc(16384);
-  th->ucp->uc_stack.ss_size=16384;
-  makecontext(th->ucp, func, 0); //make the context for a thread running func
-  th->next=NULL;
+  Queue *new_context;
+  new_context=(Queue *)malloc(sizeof(Queue));
+  new_context->context=(ucontext_t *)malloc(sizeof(ucontext_t));
+  getcontext(new_context->context); //initialize the context structure
+  new_context->context->uc_stack.ss_sp=(void *)malloc(16384);
+  new_context->context->uc_stack.ss_size=16384;
+  makecontext(new_context->context, func, 0); //make the context for a thread running func
+  new_context->next=NULL;
 
-  //add the thread record into the queue
-  if(tail!=NULL) tail->next=th;
-  else head=th;
-  tail=th;
+  return new_context;
 }
 
-void uthread_exit()
+Queue* dequeue_context()
 {
-  struct thread_info *th;
+  if(head==NULL) return NULL;
+  
+  Queue *temp;
+  temp = head;
 
-  if(head==NULL) exit(0); //all threads are finished
+  head = head->next;
 
-  //pick up the thread record at the front of the queue
-  th=head;
-  head=head->next;
-  if(head==0) tail=NULL;
 
-  //set the context of the picked thread as the concurrent thread (i.e., run it!)
-  setcontext(th->ucp);
+  return temp;
+
 }
 
-void uthread_yield()
+void enqueue_context(Queue *context){
+  if(head == NULL){
+    head = context;
+    return;
+  }else{
+    Queue *temp = head;
+
+    while(temp->next != NULL){
+      temp = temp->next;
+    }
+
+    temp->next = context;
+  }
+
+  return;
+  
+}
+
+void yield()
 {
   if(head==NULL) return;
 
-  //construct a thread record th 
-  struct thread_info *th; 
-  th=(struct thread_info *)malloc(sizeof(struct thread_info)); 
-  th->ucp=(ucontext_t *)malloc(sizeof(ucontext_t));
+  Queue *context = dequeue_context();
 
-  //apend the record to the queue
-  if(tail!=NULL) tail->next=th; 
-  else head=th; 
-  tail=th;
 
-  //fectch the front thread record th1
-  struct thread_info *th1;
-  th1=head;
-  head=head->next;
+  Queue *th; 
+  th=(Queue *)malloc(sizeof(Queue)); 
+  th->context=(ucontext_t *)malloc(sizeof(ucontext_t));
 
-  //swap th1 with th
-  swapcontext(th->ucp, th1->ucp);
+
+  Queue *temp_head = head;
+
+  enqueue_context(th);
+  swapcontext(th->context, temp_head->context);
+
 }
